5292a71db2243f03d766247c1a0675eed6cebafa
Vaibhav Choudhary <vaibhavch29@gmail.com>
2022-09-16 20:46:22 +0530

    completed validation & submission

--- scripts/api-client.js
+++ scripts/api-client.js
@@ -3,51 +3,21 @@
 const API_Client = {
     isEmailValid : false,
     
-    Validation(mail_id){
+    async Validation(mail_id){
 
-        let params = new URLSearchParams({
-            api_key: SECRET.API_KEY,
-            email: mail_id,
-          });
+        const api_key = SECRET.API_KEY;
           
-        const promise = fetch(`https://emailvalidation.abstractapi.com/v1/?${params}`);
+        const promise = fetch(`https://sleepy-wildwood-70687.herokuapp.com/https://api.quickemailverification.com/v1/verify?email=${mail_id}&apikey=${api_key}`);
 
-        promise.then(response => {
-            response.json().then(result =>{
-                console.log('Result is ', result.deliverability);
-
-                if(result.deliverability == 'DELIVERABLE'){
-                    this.isEmailValid = true;
-                }else{
-                    this.isEmailValid = false;
-                }
-                console.log(this.isEmailValid);
-
-            }).catch(err =>{
-                console.log("Error in json parsing ", err);
-            })
-        }).catch(err => {
-            console.log("Error in API call ",err);
-        })
+        return promise;
     },
 
-    Submission(formDataObj){
+    async Submission(formDataObj){
+        
         const scriptURL = SECRET.SCRIPT_URL;
-        const form = document.forms['google-sheet']
-        const promise = fetch(scriptURL, {
-            method: 'POST',
-            headers: {
-                
-            },
-            body: new FormData(form) })
-        
-        form.addEventListener('submit', onsubmitSuccess => {
-            onsubmitSuccess.preventDefault();
-            console.log("form submission success");
-            promise
-                .catch(error => console.error('Error!', error.message))
-        })
-        
 
+        const promise = fetch(scriptURL, { method: 'POST', body: formDataObj });
+
+        return promise;
     },
 }
